389821f2f29b92345823369b1e7f68648c48d243
Madu Chika <madusonovidius@gmail.com>
2020-05-08 16:19:26 +0100

    lab5

--- myscrumy/settings.py
+++ myscrumy/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'linuxjobber',
+        'USER': 'root',
+        'PASSWORD': 'queeneth72',
     }
 }
 
