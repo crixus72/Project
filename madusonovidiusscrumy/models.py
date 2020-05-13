ada875daf3d0f659202a009f2029ac3444e88656
Madu Chika <madusonovidius@gmail.com>
2020-05-13 17:36:54 +0100

    Latest working file

--- madusonovidiusscrumy/models.py
+++ madusonovidiusscrumy/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ScrumyGoals(models.model):
+    goal_name = models.TextField()
+    goal_id = models.IntegerField()
+    created_by = models.TextField()
+    moved_by = models.TextField()
+    owner = models.TextField()
+    goal_status = models.ForeignKey(
+        'GoalStatus',
+        on_delete=models.CASCADE()
+    )
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE()
+    )
+
+class ScrumyHistory(models.model):
+    moved_by = models.TextField
+    created_by = models.TextField
+    moved_from = models.TextField
+    moved_to = models.TextField
+    time_of_action = models.DateTimeField
+
+class GoalStatus(models.model):
+    status_name = models.TextField()
+
+
+
