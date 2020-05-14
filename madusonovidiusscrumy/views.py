9fcb1b963d52a95d35b11ff586fae05023835225
Madu Chika <madusonovidius@gmail.com>
2020-05-14 02:38:40 +0100

    Latest working file

--- madusonovidiusscrumy/views.py
+++ madusonovidiusscrumy/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def get_grading_parameters(request):
-    return HttpResponse("<h1>This is a Scrum application</h1>")
+    return HttpResponse("This is a Scrum application")
