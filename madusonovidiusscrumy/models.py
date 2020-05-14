5f1132d7141e7ad65fce27efee0ece72e7f98973
Madu Chika <madusonovidius@gmail.com>
2020-05-14 02:26:19 +0100

    Latest working file

--- madusonovidiusscrumy/models.py
+++ madusonovidiusscrumy/models.py
@@ -3,27 +3,29 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class ScrumyGoals(models.model):
-    goal_name = models.CharField()
+class GoalStatus(models.Model):
+    status_name = models.TextField()
+
+
+class ScrumyGoals(models.Model):
+    goal_name = models.TextField()
     goal_id = models.IntegerField()
-    created_by = models.CharField()
-    moved_by = models.CharField()
-    owner = models.CharField()
-    goal_status = models.ForeignKey('GoalStatus', on_delete=models.PROTECT())
-    user = models.ForeignKey('User', on_delete=models.CASCADE())
+    created_by = models.TextField()
+    moved_by = models.TextField()
+    owner = models.TextField()
+    goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT)
+    user = models.ForeignKey('User',
+        on_delete=models.CASCADE, related_name='user+',)
 
 
-class ScrumyHistory(models.model):
-    moved_by = models.CharField
-    created_by = models.CharField
-    moved_from = models.CharField
-    moved_to = models.CharField
+class ScrumyHistory(models.Model):
+    moved_by = models.TextField
+    created_by = models.TextField
+    moved_from = models.TextField
+    moved_to = models.TextField
     time_of_action = models.DateTimeField
-    goal = models.ForeignKey(ScrumyGoals)
+    goal = models.ForeignKey(ScrumyGoals, on_delete=models.CASCADE)
 
 
-class GoalStatus(models.model):
-    status_name = models.CharField()
-
-
-
+class User(models.Model):
+    pass
