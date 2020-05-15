5a3fafb305784cea3184f202eaa5eed1af8d7f93
Madu Chika <madusonovidius@gmail.com>
2020-05-15 23:38:09 +0100

    Latest working file

--- madusonovidiusscrumy/models.py
+++ madusonovidiusscrumy/models.py
@@ -1,29 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 # Create your models here.
 class GoalStatus(models.Model):
-    STATUS_CHOICES = (
-        ('pending', 'Pending',),
-        ('reviewing', 'Reviewing'),
-        ('approval', 'Approval'),
-    )
-    status_name = models.CharField(max_length=15,
-                                   choices=STATUS_CHOICES,
-                                   default='pending')
+    status_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.status_name
 
 
 class ScrumyGoals(models.Model):
     goal_name = models.CharField(max_length=50)
-    goal_id = models.IntegerField(default=1)
+    goal_id = models.IntegerField()
     created_by = models.CharField(max_length=50)
     moved_by = models.CharField(max_length=50)
     owner = models.CharField(max_length=50)
     goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT)
     user = models.ForeignKey(User,
-                             on_delete=models.PROTECT,
-                             related_name='created_by',)
+                             related_name='goal_created',
+                             on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.goal_name
 
 
 class ScrumyHistory(models.Model):
@@ -31,6 +31,10 @@ class ScrumyHistory(models.Model):
     created_by = models.CharField(max_length=50)
     moved_from = models.CharField(max_length=50)
     moved_to = models.CharField(max_length=50)
-    time_of_action = models.DateTimeField(auto_now_add=True)
+    time_of_action = models.DateTimeField(timezone.now)
     goal = models.ForeignKey(ScrumyGoals, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.created_by
+
 
--- madusonovidiusscrumy/views.py
+++ madusonovidiusscrumy/views.py
@@ -1,8 +1,10 @@
 #Django
 from django.shortcuts import render
 from django.http import HttpResponse
+from madusonovidiusscrumy.models import GoalStatus
 
 
 # Create your views here.
 def get_grading_parameters(request):
-    return HttpResponse("This is a Scrum Application")
+    result = GoalStatus.objects.filter(status_name__exact='Weekly Goal')
+    return HttpResponse(result)
