9894b30fbee206e46f10b1b9028426c379947f01
Madu Chika <madusonovidius@gmail.com>
2020-05-16 23:12:30 +0100

    Latest working file

--- django-madusonovidiusscrumy/madusonovidiusscrumy/models.py
+++ django-madusonovidiusscrumy/madusonovidiusscrumy/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class GoalStatus(model.Model):
-status_name = models.CharField(max_length=50)
-
-
-class ScrumyGoals(models.Model):
-	goal_name = models.CharField(max_length=250)
-	goal_id = models.IntegerField
-	created_by = models.
-
-
-class ScrumyHistory(models.Model):
 
 
 
--- madusonovidiusscrumy/views.py
+++ madusonovidiusscrumy/views.py
@@ -1,9 +1,11 @@
 #Django
 from django.http import HttpResponse
+from madusonovidiusscrumy.models import ScrumyGoals
 
 
 def get_grading_parameters(request):
-    return HttpResponse("This is a Scrum Application")
+    response = ScrumyGoals.objects.filter(goal_name__exact='Learn Django')
+    return HttpResponse(response)
 
 
 # Create your views here.
