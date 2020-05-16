bca5bee04a2844ac31014452a9e4c6470c2d0d7c
Madu Chika <madusonovidius@gmail.com>
2020-05-17 00:57:39 +0100

    Latest working file

--- madusonovidiusscrumy/views.py
+++ madusonovidiusscrumy/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 from madusonovidiusscrumy.models import ScrumyGoals
 
 
-def get_grading_parameters(request):
-    response = ScrumyGoals.objects.filter(goal_name__exact='Learn Django')
-    return HttpResponse(response)
+#def get_grading_parameters(request):
+    #response = ScrumyGoals.objects.filter(goal_name__exact='Learn Django')
+    #return HttpResponse(response)
 
+
+def move_goal(request, goal_id):
+    answer = ScrumyGoals.objects.get(id=goal_id)
+    return HttpResponse(answer)
 
 # Create your views here.
