089b4952873188650f12c69d8f40f5706abd94d1
Madu Chika <madusonovidius@gmail.com>
2020-05-17 01:02:36 +0100

    Latest working file

--- madusonovidiusscrumy/urls.py
+++ madusonovidiusscrumy/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 urlpatterns = [
     #path('', views.get_grading_parameters, name='get_grading_parameters'),
-    path('<int:goal_id>/', views.move_goal, name='move_goal')
+    path('movegoal/<int:goal_id>/', views.move_goal, name='move_goal')
 ]
 
