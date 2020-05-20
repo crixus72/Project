eb83b84bb9e92f4040e6a559fad1229d1bf70798
Madu Chika <madusonovidius@gmail.com>
2020-05-20 08:25:08 +0100

    Latest working file

--- madusonovidiusscrumy/urls.py
+++ madusonovidiusscrumy/urls.py
@@ -6,6 +6,9 @@ from . import views
 
 urlpatterns = [
     #path('', views.get_grading_parameters, name='get_grading_parameters'),
-    path('movegoal/<int:goal_id>/', views.move_goal, name='move_goal')
+    path('movegoal/<int:goal_id>/', views.move_goal, name='move_goal'),
+    path('addgoal', views.add_goal, name='add_goal'),
+    path('home', views.home, name='home'),
+
 ]
 
--- madusonovidiusscrumy/views.py
+++ madusonovidiusscrumy/views.py
@@ -1,15 +1,40 @@
-#Django
+# Django
 from django.http import HttpResponse
-from madusonovidiusscrumy.models import ScrumyGoals
+from madusonovidiusscrumy.models import *
+from django.contrib.auth.models import User
+from random import randint
 
 
-#def get_grading_parameters(request):
-    #response = ScrumyGoals.objects.filter(goal_name__exact='Learn Django')
-    #return HttpResponse(response)
+# def get_grading_parameters(request):
+# response = ScrumyGoals.objects.filter(goal_name__exact='Learn Django')
+# return HttpResponse(response)
 
 
 def move_goal(request, goal_id):
     answer = ScrumyGoals.objects.get(id=goal_id)
     return HttpResponse(answer)
+
+
+def add_goal(request):
+    already_used = []
+    number = randint(1000, 9999)
+    if number not in already_used:
+        addgoal = ScrumyGoals.objects.create(goal_name='Keep Learning Django',
+                                             goal_id=number,
+                                             created_by='Louis',
+                                             moved_by='Louis', owner='Louis',
+                                             goal_status=GoalStatus.objects.get
+                                             (status_name='Weekly Goal'),
+                                             user=User.objects.get(
+                                                 username='Louis Oma')
+                                             )
+        already_used.append(number)
+    return HttpResponse(addgoal)
+
+def home (request):
+    show_record = ScrumyGoals.objects.filter(goal_name__exact=
+                                             'Keep Learning Django')
+    output = ', '.join([a.goal_name for a in show_record])
+    return HttpResponse(output)
 
 # Create your views here.
