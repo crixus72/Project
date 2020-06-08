e47503cef389dbb2e3261eb0bedb6ac30ec012b3
Madu Chika <madusonovidius@gmail.com>
2020-06-08 16:36:10 +0100

    Latest working file

--- websocket/urls.py
+++ websocket/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from websocket import views
 
+app_name = 'websocket'
+
 urlpatterns = [
     path('test/', views.test, name='test'),
     path('disconnect/', views.disconnect, name='disconnect'),
