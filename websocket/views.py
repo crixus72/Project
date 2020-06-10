5d2ee129c7a30bc796ab6192493ce3dff1e5d0ab
Madu Chika <madusonovidius@gmail.com>
2020-06-10 21:04:55 +0100

    Latest working file

--- websocket/views.py
+++ websocket/views.py
@@ -72,4 +72,4 @@ def recent_messages(request):
                              "message": message.message,
                             "timestamp": message.timestamp,
                              })
-        print({'messages': sorted(recent_texts)})
+    print({'messages': sorted(recent_texts)})
